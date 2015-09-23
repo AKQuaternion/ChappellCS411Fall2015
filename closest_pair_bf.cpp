00df5d7fdf8b2873d135cc6916a21e18f5efdb13
Glenn G. Chappell <ggchappell@alaska.edu>
2015-09-23 11:03:02 -0800

    Add closest_pair_bf.cpp (UNFINISHED)

---
+++ closest_pair_bf.cpp
@@ -0,0 +1,183 @@
+// closest_pair_bf.cpp  UNFINISHED
+// Glenn G. Chappell
+// 23 Sep 2015
+//
+// For CS 411/611 Fall 2015
+// Brute-Force Closest-Pair
+
+#include "grand.h"
+// For class GRand - easy pseuorandom number generation
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include <vector>
+using std::vector;
+#include <utility>
+using std::pair;
+using std::make_pair;
+#include <cmath>
+using std::sqrt;
+#include <cstdlib>
+using std::size_t;
+using std::exit;
+#include <string>
+using std::string;
+using std::getline;
+#include <sstream>
+using std::istringstream;
+#include <algorithm>
+using std::swap;
+#include <cassert>  // for assert
+
+
+// getNum
+// Prints prompt to cout and then inputs a number of type int on a line
+// from cin. Repeats until valid number obtained; returns it to caller
+// in reference argument. Return value is false if number could not be
+// obtained.
+bool getNum(const string & prompt,
+            int & num)
+{
+    while (true)
+    {
+        cout << prompt;
+        string line;
+        getline(cin, line);
+        if (!cin)
+        {
+            if (cin.eof())
+                return false;
+            continue;
+        }
+
+        istringstream istr(line);
+        istr >> num;
+        if (!istr)
+            continue;
+        return true;
+    }
+}
+
+
+// struct Pt2
+// Holds a 2-D point in the obvious way
+struct Pt2 {
+    Pt2(double theX, double theY)
+        :x(theX),
+         y(theY)
+    {}
+
+    double x;
+    double y;
+};
+
+
+// dist
+// Given two Pt2 objects, returns the distance between them.
+double dist(const Pt2 & p1,
+            const Pt2 & p2)
+{
+    double xd = p1.x - p2.x;
+    double yd = p1.y - p2.y;
+    return sqrt(xd*xd + yd*yd);
+}
+
+
+// randPt2
+// Returns a random Pt2 object, both coordinates in range [min, max],
+//  uniformly distributed, based on given PRNG.
+Pt2 randPt2(GRand & r,
+            double min=0.,
+            double max=1.)
+{
+    double x = r.d(max-min)+min;
+    double y = r.d(max-min)+min;
+    return Pt2(x, y);
+}
+
+
+// printPair
+// Given a list of Pt2 objects and a pair of indices in this list,
+// output the points with the given indices, in order of increasing
+// index, along with the distance between the two points.
+void printPair(const vector<Pt2> & pts,
+               pair<size_t, size_t> p)
+{
+    size_t a = p.first;
+    size_t b = p.second;
+    if (b < a)
+        swap(a, b);
+    cout << a << ": (" << pts[a].x << ", " << pts[a].y << ")" << endl;
+    cout << b << ": (" << pts[b].x << ", " << pts[b].y << ")" << endl;
+    cout << "Distance: " << dist(pts[a], pts[b]) << endl;
+    cout << endl;
+}
+
+
+// bfClosestPair
+// Given vector of Pt2 objects, returns a std::pair holding the indices
+//  of a closest pair of distinct points.
+// Uses brute-force method.
+// Pre:
+//     pts.size() >= 2.
+pair<size_t, size_t> bfClosestPair(const vector<Pt2> & pts)
+{
+    // WRITE THIS!!!
+
+    // Return indices of closest pair as std::pair<size_t,size_t>
+    return make_pair(size_t(0), size_t(1));  // DUMMY RETURN
+}
+
+
+// main
+// Generate list of random 2-D pts, find closest pair, print results.
+int main()
+{
+    GRand r;  // Our PRNG; seeded unpredictably
+
+    // Get number of points
+    int num_pts;
+    while (true)
+    {
+        bool success = getNum("Number of points (at least 2)? ", num_pts);
+        if (!success)
+            exit(1);
+        if (num_pts >= 2)
+            break;
+        cout << endl;
+        cout << "Number of points must be at least 2" << endl;
+    }
+    cout << endl;
+
+    // Generate list of points
+    vector<Pt2> pts;
+    for (int i = 0; i < num_pts; ++i)
+    {
+        pts.push_back(randPt2(r, -100., 100.));
+    }
+
+    /*
+    // Print list of points
+    cout << num_pts << " points:" << endl;
+    for (int i = 0; i < num_pts; ++i)
+    {
+        cout << i << ": (" << pts[i].x << ", " << pts[i].y << ")"
+             << endl;
+    }
+    cout << endl;
+    */
+    cout << num_pts << " points generated" << endl;
+    cout << endl;
+
+    // Print closest pair (Brute-Force method)
+    cout << "Closest pair (Brute-Force method):" << endl;
+    auto closest_bf = bfClosestPair(pts);
+    printPair(pts, closest_bf);
+
+    cout << "Press ENTER to quit ";
+    while (cin.get() != '\n') ;
+
+    return 0;
+}
+
